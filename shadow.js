d0e08735f8f2181d155cee047c6fc785da8471a7
James Purnell <jamespurnell27@hotmail.com>
2016-11-16 23:50:42 +1100

    Fixed get_XY equations

--- shadow.js
+++ shadow.js
@@ -1,18 +1,76 @@
 window.onload = function() {
 
+  //Pub and Sub object///////////////////////////////////////////
+  let pubSub = {
+    events: {},
+
+    on: function( eventName, fn ) {
+      this.events[eventName] = this.events[eventName] || []
+      this.events[eventName].push( fn )
+    },
+
+    off: function( eventName ) {
+      if( this.events[eventName] ) {
+        let eventLength = this.events.length;
+        for( let i = 0; i < eventLength; i++ ) {
+          this.events.splice( i,1 );
+          break
+        }
+      }
+    },
+
+    emit: function( eventName, data ) {
+      if( this.events[eventName] ) {
+        this.events[eventName].forEach( function( fn ) {
+          fn( data )
+        })
+      }
+    }
+
+  }; //End of Pub and Sub Object////////////////////////////////////
+
   //function factory
   let shadow = function( element, info ) {
 
-      let state = {
-        middleY: info.top + ( info.height/2 ),
-        middleX: info.left + ( info.width/2 ),
+    let state = {
+      middleY: info.top + ( info.height/2 ),
+      middleX: info.left + ( info.width/2 ),
+      elemHeight: info.height,
+      elemWidth: info.width
+    }
+
+    let newX, newY, blur
+
+    function get_XY( m, b, b_WH ) {
+
+      let xy
+      console.log('m: ' + m);
+      console.log('b: ' + b);
+
+      if( ( m - b ) >= 200 ) {
+        xy = 1 * ( b_WH * -0.75 )
+      }else if ( ( m - b ) <= -200 ) {
+        xy = 1 * ( b_WH * 0.75 )
+      }else {
+        xy = m < b ? ( ( b - m ) / 200 ) * ( b_WH * 0.55 ) : ( ( m - b ) / 200 ) * ( b_WH * -0.55 )
       }
 
-      return {
-        consoley: function() {
-          console.log(state);
-        }
+      return xy
+
+    }
+
+    return {
+      action: function(mouse) {
+        newX = get_XY( mouse.x, state.middleX, state.elemWidth )
+        newY = get_XY( mouse.y, state.middleY, state.elemHeight )
+
+        console.log('y: ' + newY)
+
+        element.style.boxShadow = newX + 'px ' + newY + 'px 50px 20px #666';
+        //element.style.boxShadow = newX + 'px ' + newY + 'px BLUR px SPREAD #666';
+
       }
+    }
 
   }//END function factory
 
@@ -27,14 +85,23 @@ window.onload = function() {
     // Create new object
     let newShadow = shadow( element,info )
     //Push into array
-    shadowElements.push(newShadow) 
+    shadowElements.push( newShadow )
+    console.log(index)
+    pubSub.on( "mousePos", shadowElements[index].action );
 
   })
 
-  console.log(shadowElements)
-  console.log(shadowElements[0].consoley())
 
+  //Track mouse movement and emit
+  document.addEventListener("mousemove", mousePos)
 
+  function mousePos(e) {
+    let mouse = {
+      x: e.clientX,
+      y: e.clientY
+    }
+    pubSub.emit( "mousePos", mouse )
+  }
 
 };
 
